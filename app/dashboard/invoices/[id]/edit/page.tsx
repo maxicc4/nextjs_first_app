abe1061c34402f23070fb5452aa0bc6415843c3d
Maxi Burastero <mburastero@inase.gob.ar>
2025-09-03 13:43:31 -0300

    Chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import {fetchInvoiceById, fetchCustomers} from '@/app/lib/data';
+import {notFound} from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -10,11 +11,15 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchCustomers(),
     ]);
 
+    if (!invoice) {
+        notFound();
+    }
+
     return (
         <main>
             <Breadcrumbs
                 breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {label: 'Invoices', href: '/dashboard/invoices'},
                     {
                         label: 'Edit Invoice',
                         href: `/dashboard/invoices/${id}/edit`,
@@ -22,7 +27,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
                     },
                 ]}
             />
-            <Form invoice={invoice} customers={customers} />
+            <Form invoice={invoice} customers={customers}/>
         </main>
     );
 }
